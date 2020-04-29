55ab012edad7db765efdae7b4c14fa0e0cb1b386
HBalija <balija.hrvoje@gmail.com>
2020-04-29 15:12:20 +0200

    Add first ws server message handlers

--- server/go.mod
+++ server/go.mod
@@ -2,4 +2,7 @@ module github.com/HBalija/pink-irc
 
 go 1.14
 
-require github.com/gorilla/websocket v1.4.2
+require (
+	github.com/gorilla/websocket v1.4.2
+	github.com/mitchellh/mapstructure v1.3.0
+)
--- server/main.go
+++ server/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/websocket"
+	"github.com/mitchellh/mapstructure"
 )
 
 // upgrader: switch connection from http to websocket
@@ -15,9 +17,20 @@ var upgrader = websocket.Upgrader{
 	CheckOrigin:     func(r *http.Request) bool { return true },
 }
 
+// Message received structure
+type Message struct {
+	Name string      `json:"name"`
+	Data interface{} `json:"data"`
+}
+
+// Channel message data structure
+type Channel struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
 func main() {
 	http.HandleFunc("/", handler)
-
 	http.ListenAndServe(":8000", nil)
 }
 
@@ -31,20 +44,52 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	for {
-		msgType, msg, err := socket.ReadMessage()
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		fmt.Println("msgType", msgType)
-		// msg --> []slice
-		fmt.Println("msg", string(msg))
+		var inMessage Message
+		var outMessage Message
 
-		// echo message back to the client
-		err = socket.WriteMessage(msgType, msg)
+		err := socket.ReadJSON(&inMessage)
 		if err != nil {
 			fmt.Println(err)
-			return
+			break
 		}
+
+		fmt.Printf("%#v\n", inMessage)
+
+		switch inMessage.Name {
+		case "channel add":
+			err := addChannel(inMessage.Data)
+			if err != nil {
+				outMessage = Message{Name: "error", Data: err}
+				err := socket.WriteJSON(outMessage)
+				if err != nil {
+					fmt.Println(err)
+					break
+				}
+			}
+		case "channel subscribe":
+			go subscribeChannel(socket)
+		}
+	}
+}
+
+func addChannel(data interface{}) error {
+	var channel Channel
+
+	err := mapstructure.Decode(data, &channel)
+	if err != nil {
+		return err
+	}
+	channel.ID = "1"
+	fmt.Println("added chennel")
+	return nil
+}
+
+func subscribeChannel(socket *websocket.Conn) {
+	// TODO: rethinkDB Query  / changefeed
+	for {
+		time.Sleep(time.Second * 1)
+		message := Message{Name: "channel add", Data: Channel{ID: "1", Name: "Software Support"}}
+		socket.WriteJSON(message)
+		fmt.Println("sent new message")
 	}
 }
